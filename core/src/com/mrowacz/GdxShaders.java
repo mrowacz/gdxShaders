61de2394d1ea1e64e48fb05e85734088eca9b294
Łukasz Czerwiński <mrowacz@gmail.com>
2017-09-28 23:54:54 +0200

    add shader for badlogic.jpg
    Signed-off-by: Łukasz Czerwiński <mrowacz@gmail.com>

--- core/src/com/mrowacz/GdxShaders.java
+++ core/src/com/mrowacz/GdxShaders.java
@@ -5,15 +5,24 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
 public class GdxShaders extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	String vertexShader;
+	String fragmentShader;
+	ShaderProgram shaderProgram;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+
+		vertexShader = Gdx.files.internal("vertex.glsl").readString();
+		fragmentShader = Gdx.files.internal("fragment.glsl").readString();
+		shaderProgram = new ShaderProgram(vertexShader,fragmentShader);
 	}
 
 	@Override
@@ -21,6 +30,7 @@ public class GdxShaders extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		batch.setShader(shaderProgram);
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
