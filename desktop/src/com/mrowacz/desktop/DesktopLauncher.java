fc8be3146968facb7b195049801fd8a69300d013
Łukasz Czerwiński <mrowacz@gmail.com>
2017-09-28 23:48:24 +0200

    init repo
    Signed-off-by: Łukasz Czerwiński <mrowacz@gmail.com>

---
+++ desktop/src/com/mrowacz/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mrowacz.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mrowacz.GdxShaders;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxShaders(), config);
+	}
+}
